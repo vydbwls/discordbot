322469bb50bc169ed53d5af0f09ff40926b923ca
vydbwls <81695239+vydbwls@users.noreply.github.com>
2025-01-30 20:27:43 +0900

    Update discordbot_Li.py

--- discordbot_Li.py
+++ discordbot_Li.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 from dotenv import load_dotenv
 
-load_dotenv()  # .env 파일 로드
+load_dotenv("discord_Li_token.env")  # .env 파일 로드
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")  # 환경 변수에서 토큰 가져오기
 
 intents = discord.Intents.default()
